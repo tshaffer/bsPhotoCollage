f1be8a37cdc92be043e209c76dd67760c4815109
Ted Shaffer <ted@brightsign.biz>
2020-10-31 05:07:25 -0700

    Additional necessary files.

--- src/component/BsPhotoCollage.tsx
+++ src/component/BsPhotoCollage.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import * as ReactDOM from 'react-dom';
 import { Dispatch, bindActionCreators } from 'redux';
+const fs = require('fs');
 
 import {
   BsPhotoCollageState,
@@ -29,6 +29,10 @@ class BsPhotoCollageComponent extends React.Component<
   setCanvasRef: any;
   ctx: any;
 
+  fileWritten: boolean;
+  natureDrawn: boolean;
+  lionDrawn: boolean;
+
   constructor(props: any) {
     super(props);
 
@@ -40,6 +44,10 @@ class BsPhotoCollageComponent extends React.Component<
       this.canvasRef = element;
       this.ctx = element.getContext('2d');
     };
+
+    this.fileWritten = false;
+    this.natureDrawn = false;
+    this.lionDrawn = false;
   }
 
   componentDidMount() {
@@ -53,9 +61,23 @@ class BsPhotoCollageComponent extends React.Component<
     });
   }
 
-  imageLoaded() {
-
+  // 
+  writeFile() {
+    if (!this.fileWritten) {
+      // const img1 = this.ctx.getImageData(0, 0, 971, 420);
+      // const data = img1.data;
+      // const buffer = new ArrayBuffer(data.length);
+      // const binary = new Uint8Array(buffer);
+      // for (let i = 0; i < binary.length; i++) {
+      //   binary[i] = data[i];
+      // }
+      // // const buffer = imageData.data.buffer;
+      // // const buffer = this.canvasRef.toBuffer('image/png');
+      // fs.writeFileSync('./flibbet.png', binary);
+      // this.fileWritten = true;
+    }
   }
+
   render() {
 
     console.log('render');
@@ -70,12 +92,22 @@ class BsPhotoCollageComponent extends React.Component<
       const natureImage = new Image();
       natureImage.onload = () => {
         context.drawImage(natureImage, 0, 0);
+        if (this.lionDrawn) {
+          this.writeFile();
+        } else {
+          this.natureDrawn = true;
+        }
       };
       natureImage.src = 'nature.jpg';
 
       const lionImage = new Image();
       lionImage.onload = () => {
         context.drawImage(lionImage, 551, 0);
+        if (this.natureDrawn) {
+          this.writeFile();
+        } else {
+          this.lionDrawn = true;
+        }
       };
       lionImage.src = 'lion.png';
 
