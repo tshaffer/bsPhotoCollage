a98f1c876de4718f79ba76f4f2631ee06d59ded2
Ted Shaffer <ted@brightsign.biz>
2020-11-21 07:55:23 -0800

    removed unnecessary code - not complete.

--- src/component/PhotoCollage.tsx
+++ src/component/PhotoCollage.tsx
@@ -102,56 +102,6 @@ class PhotoCollageComponent extends React.Component<
     this.intervalId = setInterval(this.handleTimeout, 10000, timeoutEventCallbackParams);
   }
 
-  getRandomInt(max: number): number {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
-
-  getPhoto(landscape: boolean): PhotoInCollection {
-
-    const photosInCollection: PhotoInCollection[] = this.props.photoCollection.photosInCollection!;
-    const numPhotos = photosInCollection.length;
-
-    while (true) {
-      const randomInt = this.getRandomInt(numPhotos);
-      const photoInCollection: PhotoInCollection = photosInCollection[randomInt];
-      if (!isNil(photoInCollection.height)) {
-        const landscapeOrientation: boolean = photoInCollection.width! >= photoInCollection.height;
-        if (landscape === landscapeOrientation) {
-          const filePath: string = getFilePathFromPhotoInCollection(this.props.photosRootDirectory, photoInCollection);
-          if (fs.pathExistsSync(filePath)) {
-            return photoInCollection;
-          }
-        }
-      }
-    }
-  }
-
-  renderPhoto(filePath: string, x: number, y: number, width: number, height: number) {
-    const photo = new Image();
-    photo.onload = () => {
-      this.scaleToFit(photo, x, y, width, height);
-    };
-    photo.src = filePath;
-  }
-
-  scaleToFit(photo: any, xOnCanvas: number, yOnCanvas: number, widthOnCanvas: number, heightOnCanvas: number) {
-    const scale = Math.min(widthOnCanvas / photo.width, heightOnCanvas / photo.height);
-    const x = (widthOnCanvas / 2) - (photo.width / 2) * scale;
-    const y = (heightOnCanvas / 2) - (photo.height / 2) * scale;
-    this.ctx.drawImage(photo, x + xOnCanvas, y + yOnCanvas, photo.width * scale, photo.height * scale);
-  }
-
-  getScaledCoordinates(x: number, y: number, width: number, height: number, collageWidth: number, collageHeight: number, screenWidth: number, screenHeight: number): any {
-    const screenX = (x / collageWidth) * screenWidth;
-    const screenY = (y / collageHeight) * screenHeight;
-    return {
-      x: screenX,
-      y: screenY,
-      width: (width / collageWidth) * screenWidth,
-      height: (height / collageHeight) * screenHeight,
-    };
-  }
-
   handleTimeout(photoCollageComponent: any) {
     console.log('timeoutHandler invoked');
     console.log(photoCollageComponent.photoCollageComponent);
@@ -202,54 +152,7 @@ class PhotoCollageComponent extends React.Component<
 
   handleCloseModal() {
     this.setState({ showModal: false });
-    this.startTimer();
-  }
-
-  renderPhotosInCollage() {
-    this.photoImages = [];
-    const { collageWidth, collageHeight, photosInCollageSpecs } = this.props.photoCollageSpec!;
-    for (const photosInCollageSpec of photosInCollageSpecs) {
-      const { x, y, width, height } = photosInCollageSpec;
-
-      const photoInCollection: PhotoInCollection = this.getPhoto(width >= height);
-      console.log('photo: ', photoInCollection);
-      console.log(this.props.photoCollection);
-      // TEDTODO
-      const filePath: string = getRelativeFilePathFromPhotoInCollection(this.props.photosRootDirectory, photoInCollection);
-      // const filePath: string = getFilePathFromPhotoInCollection(this.props.photosRootDirectory, photoInCollection);
-
-      console.log('photo filePath:');
-      console.log(filePath);
-
-
-      const screenCoordinates = this.getScaledCoordinates(x, y, width, height, collageWidth, collageHeight, photoCollageConfig.width, photoCollageConfig.height);
-
-      this.photoImages.push({
-        x: screenCoordinates.x,
-        y: screenCoordinates.y,
-        width: screenCoordinates.width,
-        height: screenCoordinates.height,
-        photoInCollection,
-      });
-
-      this.renderPhoto(
-        'file:///' + filePath,
-        screenCoordinates.x,
-        screenCoordinates.y,
-        screenCoordinates.width,
-        screenCoordinates.height);
-    }
-  }
-
-  renderPhotoCollage() {
-    if (isNil(this.props.photoCollageSpec) ||
-      isNil(this.props.photoCollection) ||
-      isNil(this.props.photoCollection!.photosInCollection) ||
-      this.props.photoCollection.photosInCollection.length === 0) {
-      console.log('no photoCollageSpec or no photosInCollection');
-      return;
-    }
-    this.renderPhotosInCollage();
+    // this.startTimer();
   }
 
   renderDialog(): any {
@@ -279,14 +182,6 @@ class PhotoCollageComponent extends React.Component<
     console.log('render');
     console.log(this.canvasRef);
     console.log(this.state.imageCount);
-
-    if (!isNil(this.canvasRef) && !isNil(this.ctx)) {
-      const context = this.ctx;
-      context.imageSmoothingEnabled = false;
-      context.clearRect(0, 0, this.canvasRef.width, this.canvasRef.height);
-
-      this.renderPhotoCollage();
-    }
 
     return (
       <div>
